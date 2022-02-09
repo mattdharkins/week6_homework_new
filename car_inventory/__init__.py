33c3ea8bfd5ed1160461db22b4bdf4cc5c663b83
mattdharkins <97219656+mattdharkins@users.noreply.github.com>
2022-02-09 11:16:49 -0600

    update
    this is actually days 1 and 2

---
+++ car_inventory/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+from config import Config
+from .site.routes import site
+from .api.routes import api
+from .authentication.routes import auth
+from .models import db as root_db
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+app.register_blueprint(api)
+app.register_blueprint(site)
+app.register_blueprint(auth)
+
+
+
+root_db.init_app(app)
+
+migrate = Migrate(app,root_db)
+
+
+
+# import models
